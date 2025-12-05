140aa54f0e103cabb4f22975fcb2787d2421b593
ray847 <xiachengyi21@163.com>
2025-12-05 19:10:56 +0800

    add ofSize concept

--- include/CrystalBase/concepts.h
+++ include/CrystalBase/concepts.h
@@ -1,14 +1,19 @@
 #ifndef CRYSTALBASE_CONCEPTS_H_
 #define CRYSTALBASE_CONCEPTS_H_
 
+#include <cstddef> // size_t
+
 #include <concepts>
-#include <type_traits>
+#include <type_traits> // std::convertible_to
 
 namespace crystal {
 template<typename T, typename OutputType = void>
 concept Callable = requires (T t) {
   {t()} -> std::convertible_to<OutputType>;
 };
+
+template<typename T, size_t size>
+concept ofSize = sizeof(T) == size;
 } // namespace crystal
 
 #endif
