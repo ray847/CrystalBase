2045b407e90454045650f840957f4981dc73fd90
ray847 <xiachengyi21@163.com>
2025-12-17 17:40:14 +0800

    Refine StableVector

--- include/CrystalBase/stable_vector.h
+++ include/CrystalBase/stable_vector.h
@@ -1,126 +1,140 @@
-#ifndef CRYSTALBASE_SLOT_MAP_H_
-#define CRYSTALBASE_SLOT_MAP_H_
+#ifndef CRYSTALBASE_STABLE_VECTOR_H_
+#define CRYSTALBASE_STABLE_VECTOR_H_
 
-#include <cstddef> // size_t
+#include <cstddef>
 
-#include <limits> // std::numeric_limits::max()
-#include <memory> // std::allocator
-#include <vector> // std::vector
-#include <memory_resource> // std::pmr
-#include <variant> // std::variant
-#include <utility> // std::move, std::forward
+#include <vector>
+#include <list>
+#include <memory_resource>
+#include <utility>
+#include <memory>
 
 namespace crystal {
-template<typename T, typename Allocator = std::allocator<T>>
+
+template<typename T, typename Alloc = std::allocator<T>>
 class StableVector {
 public:
-  using allocator_type = Allocator;
-  /* Constructor */
-  StableVector() : StableVector(Allocator{}) {}
-  explicit StableVector(const Allocator& allocator):
-    arr_{allocator}, free_head_(nullidx) {}
+
+  using allocator_type = Alloc;
+
+  /* Constructors */
+  StableVector() : StableVector(allocator_type{}) {}
+
+  explicit StableVector(const allocator_type& allocator):
+    arr_{allocator},
+    free_list_{allocator} {}
+
   template<typename Iter>
-  StableVector(Iter begin, Iter end, const Allocator& allocator = {}):
+  StableVector(Iter begin, Iter end, const allocator_type& allocator = {}):
     StableVector(allocator) {
     while (begin != end) {
-      (void)PushBack(*begin++); // neglecting return value
+      (void)PushBack(*begin++);
     }
   }
-  StableVector(std::initializer_list<T> lst, const Allocator& allocator = {}):
+
+  StableVector(std::initializer_list<T> lst,
+               const allocator_type& allocator = {}):
     StableVector(lst.begin(), lst.end(), allocator) {}
+
   StableVector(const StableVector& other,
-               const Allocator& allocator = {}):
-    arr_{other.arr_, allocator}, free_head_(other.free_head_) {}
+               const allocator_type& allocator = {}):
+    arr_{other.arr_, allocator},
+    free_list_{other.free_list_, allocator} {}
+
   StableVector(StableVector&&) = default;
-  StableVector(StableVector&& other, allocator_type allocator):
-    arr_{std::move(other.arr_), allocator}, free_head_(other.free_head_) {
-      other.free_head_ = nullidx;
-    }
-  /* Assignment Operator */
+
+  StableVector(StableVector&& other, const allocator_type& allocator):
+    arr_{std::move(other.arr_), allocator},
+    free_list_{std::move(other.free_list_), allocator} {}
+
+  /* Assignment Operators */
   StableVector& operator=(const StableVector& other) = default;
   StableVector& operator=(StableVector&& other) = default;
   ~StableVector() = default;
-  allocator_type get_allocator() const {return arr_.get_allocator();}
-  /* Functions */
+
+  allocator_type get_allocator() const { return arr_.get_allocator(); }
+
   /* Element Access */
-  T& At(size_t idx) {return std::get<T>(arr_.at(idx));}
-  const T& At(size_t idx) const {return std::get<T>(arr_.at(idx));}
-  T& operator[](size_t idx) {return std::get<T>(arr_[idx]);}
-  const T& operator[](size_t idx) const {return std::get<T>(arr_[idx]);}
+  T& At(size_t idx) { return arr_.at(idx); }
+  const T& At(size_t idx) const { return arr_.at(idx); }
+  T& operator[](size_t idx) { return arr_[idx]; }
+  const T& operator[](size_t idx) const { return arr_[idx]; }
+
   /* Capacity */
-  void Reserve(size_t n) {arr_.reserve(n);}
-  size_t Capacity() const {return arr_.capacity();}
+  void Reserve(size_t n) { arr_.reserve(n); }
+  size_t Capacity() const { return arr_.capacity(); }
+
   /* Modifiers */
   void Clear() {
     arr_.clear();
-    free_head_ = nullidx;
+    free_list_.clear();
   }
+
   [[nodiscard]] size_t PushBack(const T& ele) {
-    arr_.emplace_back(std::in_place_type<T>, ele);
+    arr_.emplace_back(ele);
     return arr_.size() - 1;
   }
+
   [[nodiscard]] size_t PushBack(T&& ele) {
-    arr_.emplace_back(std::in_place_type<T>, std::move(ele));
+    arr_.emplace_back(std::move(ele));
     return arr_.size() - 1;
   }
-  template<typename...Args>
+
+  template<typename... Args>
   [[nodiscard]] size_t EmplaceBack(Args&&... args) {
-    arr_.emplace_back(std::in_place_type<T>, std::forward<Args>(args)...);
+    arr_.emplace_back(std::forward<Args>(args)...);
     return arr_.size() - 1;
   }
-  [[nodiscard]] size_t Insert(const T& ele) {
-    if (free_head_ != nullidx) {
-      size_t idx = free_head_;
-      // Retrieve the next free index from the variant
-      free_head_ = std::get<size_t>(arr_[free_head_]);
-      // Assign the new element to the variant
-      arr_[idx].template emplace<T>(ele);
+
+ [[nodiscard]] size_t Insert(const T& ele) {
+    if (!free_list_.empty()) {
+      size_t idx = free_list_.front();
+      free_list_.pop_front();
+      arr_.emplace(arr_.begin() + idx, ele);
       return idx;
     } else return PushBack(ele);
   }
+
   [[nodiscard]] size_t Insert(T&& ele) {
-    if (free_head_ != nullidx) {
-      size_t idx = free_head_;
-      // Retrieve the next free index from the variant
-      free_head_ = std::get<size_t>(arr_[free_head_]);
-      // Assign the new element to the variant
-      arr_[idx].template emplace<T>(std::move(ele));
+    if (!free_list_.empty()) {
+      size_t idx = free_list_.front();
+      free_list_.pop_front();
+      arr_.emplace(arr_.begin() + idx, ele);
       return idx;
     } else return PushBack(std::move(ele));
   }
+
   template<typename...Args>
   [[nodiscard]] size_t Emplace(Args&&... args) {
-    if (free_head_ != nullidx) {
-      size_t idx = free_head_;
-      // Retrieve the next free index from the variant
-      free_head_ = std::get<size_t>(arr_[free_head_]);
-      // Construct the new element in-place within the variant
-      arr_[idx].template emplace<T>(std::forward<Args>(args)...);
+    if (!free_list_.empty()) {
+      size_t idx = free_list_.front();
+      free_list_.pop_front();
+      arr_.emplace(arr_.begin() + idx, args...);
       return idx;
     } else {
-      // If no free slots, push to back
-      arr_.emplace_back(std::in_place_type<T>, std::forward<Args>(args)...);
-      return arr_.size() - 1;
+      return EmplaceBack(args...);
     }
   }
   void Erase(size_t idx) {
-    arr_[idx].template emplace<size_t>(free_head_);
-    free_head_ = idx;
+    arr_[idx].~T();
+    free_list_.push_front(idx);
   }
+
 private:
-  using Ele = std::variant<T, size_t>; // Changed to std::variant
-  using EleAllocator =
-  typename std::allocator_traits<Allocator>::template rebind_alloc<Ele>;
-  std::vector<Ele, EleAllocator> arr_;
-  size_t free_head_;
-  static constexpr size_t nullidx = std::numeric_limits<size_t>::max();
+  std::vector<
+  T,
+  typename std::allocator_traits<Alloc>::template rebind_alloc<T>
+  > arr_;
+  std::list<
+  size_t,
+  typename std::allocator_traits<Alloc>::template rebind_alloc<size_t>
+  > free_list_;
 };
 
 namespace pmr {
-template<typename T>
-using StableVector = StableVector<T, std::pmr::polymorphic_allocator<>>;
+template <typename T>
+using StableVector = StableVector<T, std::pmr::polymorphic_allocator<T>>;
 } // namespace pmr
-
 } // namespace crystal
 
 #endif
