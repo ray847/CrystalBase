cdbc9afb64f3fc043ac47d77a199333f435bdb66
ray847 <xiachengyi21@163.com>
2025-12-17 15:58:09 +0800

    Use std::variant for stable vector for safe destrucction of elements

--- include/CrystalBase/stable_vector.h
+++ include/CrystalBase/stable_vector.h
@@ -7,6 +7,8 @@
 #include <memory> // std::allocator
 #include <vector> // std::vector
 #include <memory_resource> // std::pmr
+#include <variant> // std::variant
+#include <utility> // std::move, std::forward
 
 namespace crystal {
 template<typename T, typename Allocator = std::allocator<T>>
@@ -41,10 +43,10 @@ public:
   allocator_type get_allocator() const {return arr_.get_allocator();}
   /* Functions */
   /* Element Access */
-  T& At(size_t idx) {return arr_.at(idx).obj;}
-  const T& At(size_t idx) const {return arr_.at(idx).obj;}
-  T& operator[](size_t idx) {return arr_[idx].obj;}
-  const T& operator[](size_t idx) const {return arr_[idx].obj;}
+  T& At(size_t idx) {return std::get<T>(arr_.at(idx));}
+  const T& At(size_t idx) const {return std::get<T>(arr_.at(idx));}
+  T& operator[](size_t idx) {return std::get<T>(arr_[idx]);}
+  const T& operator[](size_t idx) const {return std::get<T>(arr_[idx]);}
   /* Capacity */
   void Reserve(size_t n) {arr_.reserve(n);}
   size_t Capacity() const {return arr_.capacity();}
@@ -54,41 +56,59 @@ public:
     free_head_ = nullidx;
   }
   [[nodiscard]] size_t PushBack(const T& ele) {
-    size_t idx = arr_.size();
-    arr_.emplace_back(ele);
-    return idx;
+    arr_.emplace_back(std::in_place_type<T>, ele);
+    return arr_.size() - 1;
+  }
+  [[nodiscard]] size_t PushBack(T&& ele) {
+    arr_.emplace_back(std::in_place_type<T>, std::move(ele));
+    return arr_.size() - 1;
   }
   template<typename...Args>
-  [[nodiscard]] size_t EmplaceBack(Args... args) {
-    return PushBack(T{args...});
+  [[nodiscard]] size_t EmplaceBack(Args&&... args) {
+    arr_.emplace_back(std::in_place_type<T>, std::forward<Args>(args)...);
+    return arr_.size() - 1;
   }
   [[nodiscard]] size_t Insert(const T& ele) {
     if (free_head_ != nullidx) {
       size_t idx = free_head_;
-      free_head_ = arr_[free_head_].nxt;
-      arr_[idx] = ele;
+      // Retrieve the next free index from the variant
+      free_head_ = std::get<size_t>(arr_[free_head_]);
+      // Assign the new element to the variant
+      arr_[idx].template emplace<T>(ele);
       return idx;
     } else return PushBack(ele);
   }
+  [[nodiscard]] size_t Insert(T&& ele) {
+    if (free_head_ != nullidx) {
+      size_t idx = free_head_;
+      // Retrieve the next free index from the variant
+      free_head_ = std::get<size_t>(arr_[free_head_]);
+      // Assign the new element to the variant
+      arr_[idx].template emplace<T>(std::move(ele));
+      return idx;
+    } else return PushBack(std::move(ele));
+  }
   template<typename...Args>
-  [[nodiscard]] size_t Emplace(Args... args) {
-    return Insert(T{args...});
+  [[nodiscard]] size_t Emplace(Args&&... args) {
+    if (free_head_ != nullidx) {
+      size_t idx = free_head_;
+      // Retrieve the next free index from the variant
+      free_head_ = std::get<size_t>(arr_[free_head_]);
+      // Construct the new element in-place within the variant
+      arr_[idx].template emplace<T>(std::forward<Args>(args)...);
+      return idx;
+    } else {
+      // If no free slots, push to back
+      arr_.emplace_back(std::in_place_type<T>, std::forward<Args>(args)...);
+      return arr_.size() - 1;
+    }
   }
   void Erase(size_t idx) {
-    arr_[idx].nxt = free_head_;
+    arr_[idx].template emplace<size_t>(free_head_);
     free_head_ = idx;
   }
 private:
-  union Ele {
-    T obj;
-    size_t nxt;
-    Ele() = default;
-    explicit Ele(const T obj) : obj(obj) {}
-    Ele& operator=(const T other_obj) {
-      obj = other_obj;
-      return *this;
-    }
-  };
+  using Ele = std::variant<T, size_t>; // Changed to std::variant
   using EleAllocator =
   typename std::allocator_traits<Allocator>::template rebind_alloc<Ele>;
   std::vector<Ele, EleAllocator> arr_;
--- main.cpp
+++ main.cpp
@@ -1,12 +1,51 @@
+#include <iostream>
+#include <format>
 #include "CrystalBase/strict_index.h"
 #include "CrystalBase/stable_vector.h"
 
+struct tmp {
+    int val;
+
+    tmp() : val(0) {
+        std::cout << "Default constructed." << std::endl;
+    }
+
+    explicit tmp(int val) : val(val) {
+        std::cout << std::format("Constructor {}.", val) << std::endl;
+    }
+
+    tmp(const tmp& other) : val(other.val) {
+        std::cout << std::format("Copy Constructor {}.", val) << std::endl;
+    }
+
+    tmp(tmp&& other) noexcept : val(other.val) {
+        // Optional: Reset other.val to indicate it was moved
+        other.val = -1; 
+        //std::cout << std::format("Move Constructor {}.", val) << std::endl;
+    }
+
+    ~tmp() {
+        std::cout << std::format("Destroy {}.", val) << std::endl;
+    }
+};
+
 int main() {
-  crystal::StableVector<int> sv{0, 1};
-  auto new_idx_2 = sv.PushBack(2);
-  crystal::StrictIdx<int> new_idx_3{sv.PushBack(3)};
-  sv.Erase(1);
-  auto new_idx_4 = sv.PushBack(4);
-  auto new_idx_5 = sv.Emplace(5);
-  return 0;
+    // Note: Initializer lists always force copies because the elements inside are const.
+    // However, vector reallocations inside will now use Moves.
+    crystal::StableVector<tmp> sv{tmp{0}, tmp{1}};
+    
+    std::cout << "\n--- Adding 2 ---" << std::endl;
+    // Will now use Move Constructor
+    auto new_idx_2 = sv.EmplaceBack(std::move(tmp{2}));
+
+    std::cout << "\n--- Adding 3 ---" << std::endl;
+    crystal::StrictIdx<tmp> new_idx_3 = sv.EmplaceBack(3);
+
+    std::cout << "\n--- Erasing ---" << std::endl;
+    sv.Erase(1);
+
+    std::cout << "\n--- Adding 4 ---" << std::endl;
+    auto new_idx_4 = sv.EmplaceBack(std::move(tmp{4}));
+    
+    return 0;
 }
