ae7227d65b11903e5f85555d09a8eeae550437f4
ray847 <xiachengyi21@163.com>
2026-01-27 13:42:21 +0800

    format changes

--- include/CrystalBase/stable_vector.h
+++ include/CrystalBase/stable_vector.h
@@ -2,42 +2,41 @@
 #define CRYSTALBASE_STABLE_VECTOR_H_
 
 #include <cstddef>
-
-#include <vector> // std::vector
+#include <memory>
 #include <memory_resource>
 #include <utility>
-#include <memory>
 #include <variant>
+#include <vector> // std::vector
 
 namespace crystal {
 
-template<typename T, typename Alloc = std::allocator<T>>
+template <typename T, typename Alloc = std::allocator<T>>
 class stable_vector {
-public:
+ public:
   using allocator_type = Alloc; // allocator aware type
 
   /* Constructors */
-  stable_vector() : stable_vector(allocator_type{}) {}
-  explicit stable_vector(const allocator_type& allocator):
-    arr_{allocator} {}
-  template<typename Iter>
-  stable_vector(Iter begin, Iter end, const allocator_type& allocator = {}):
-    stable_vector(allocator) {
-    while (begin != end) {
-      (void)push_back(*begin++);
-    }
+  stable_vector() : stable_vector(allocator_type{}) {
+  }
+  explicit stable_vector(const allocator_type& allocator) : arr_{ allocator } {
+  }
+  template <typename Iter>
+  stable_vector(Iter begin, Iter end, const allocator_type& allocator = {}) :
+      stable_vector(allocator) {
+    while (begin != end) { (void)push_back(*begin++); }
   }
   stable_vector(std::initializer_list<T> lst,
-               const allocator_type& allocator = {}):
-    stable_vector(lst.begin(), lst.end(), allocator) {}
+                const allocator_type& allocator = {}) :
+      stable_vector(lst.begin(), lst.end(), allocator) {
+  }
   stable_vector(const stable_vector& other,
-               const allocator_type& allocator = {}):
-    arr_{other.arr_, allocator},
-    free_head_{other.free_head_} {}
+                const allocator_type& allocator = {}) :
+      arr_{ other.arr_, allocator }, free_head_{ other.free_head_ } {
+  }
   stable_vector(stable_vector&&) = default;
-  stable_vector(stable_vector&& other, const allocator_type& allocator):
-    arr_{std::move(other.arr_), allocator},
-    free_head_{other.free_head_} {}
+  stable_vector(stable_vector&& other, const allocator_type& allocator) :
+      arr_{ std::move(other.arr_), allocator }, free_head_{ other.free_head_ } {
+  }
 
   /* Assignment Operators */
   stable_vector& operator=(const stable_vector& other) = default;
@@ -46,17 +45,31 @@ public:
   /* Destructor */
   ~stable_vector() = default;
 
-  allocator_type get_allocator() const {return arr_.get_allocator();}
+  allocator_type get_allocator() const {
+    return arr_.get_allocator();
+  }
 
   /* Element Access */
-  T& at(size_t idx) { return std::get<T>(arr_.at(idx)); }
-  const T& at(size_t idx) const { return std::get<T>(arr_.at(idx)); }
-  T& operator[](size_t idx) { return std::get<T>(arr_[idx]); }
-  const T& operator[](size_t idx) const { return std::get<T>(arr_[idx]); }
+  T& at(size_t idx) {
+    return std::get<T>(arr_.at(idx));
+  }
+  const T& at(size_t idx) const {
+    return std::get<T>(arr_.at(idx));
+  }
+  T& operator[](size_t idx) {
+    return std::get<T>(arr_[idx]);
+  }
+  const T& operator[](size_t idx) const {
+    return std::get<T>(arr_[idx]);
+  }
 
   /* Capacity */
-  void reserve(size_t n) { arr_.reserve(n); }
-  size_t capacity() const { return arr_.capacity(); }
+  void reserve(size_t n) {
+    arr_.reserve(n);
+  }
+  size_t capacity() const {
+    return arr_.capacity();
+  }
 
   /* Modifiers */
   void clear() {
@@ -101,7 +114,7 @@ public:
    * underlying vector. For inserting elements with vacant slot utilizaiton,
    * check `insert`.
    */
-  template<typename... Args>
+  template <typename... Args>
   [[nodiscard]] size_t emplace_back(Args&&... args) {
     arr_.emplace_back(std::in_place_type<T>, std::forward<Args>(args)...);
     return arr_.size() - 1;
@@ -116,7 +129,7 @@ public:
    * back to appending the underlying vector. For direct appending, check
    * `PushBack`.
    */
- [[nodiscard]] size_t insert(const T& ele) {
+  [[nodiscard]] size_t insert(const T& ele) {
     if (free_head_ != kNullIdx) {
       size_t idx = free_head_;
       free_head_ = std::get<size_t>(arr_[free_head_]);
@@ -152,7 +165,7 @@ public:
    * back to appending the underlying vector. For direct appending, check
    * `PushBack`.
    */
-  template<typename...Args>
+  template <typename... Args>
   [[nodiscard]] size_t emplace(Args&&... args) {
     if (free_head_ != kNullIdx) {
       size_t idx = free_head_;
@@ -164,19 +177,18 @@ public:
     }
   }
   void erase(size_t idx) {
-    //arr_[idx].~T();
+    // arr_[idx].~T();
     arr_[idx] = free_head_;
     free_head_ = idx;
   }
 
-private:
+ private:
   using ele = std::variant<T, size_t>;
   static constexpr size_t kNullIdx = -1ul;
   /* Variables */
-  std::vector<
-  ele,
-  typename std::allocator_traits<Alloc>::template rebind_alloc<ele>
-  > arr_;
+  std::vector<ele,
+              typename std::allocator_traits<Alloc>::template rebind_alloc<ele>>
+      arr_;
   size_t free_head_ = kNullIdx;
 };
 
