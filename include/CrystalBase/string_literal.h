42ff1914127810c5ceaa530869d26bcb247af600
ray847 <xiachengyi21@163.com>
2026-02-05 21:57:39 +0800

    add string literal to string and concept

--- include/CrystalBase/string_literal.h
+++ include/CrystalBase/string_literal.h
@@ -1,11 +1,15 @@
 #ifndef CRYSTALBASE_STRING_LITERAL_H_
 #define CRYSTALBASE_STRING_LITERAL_H_
 
+#include <wchar.h>
+
 #include <array>
+#include <string>
+#include <type_traits>
 
 namespace crystal {
 
-using std::array;
+using std::array, std::string;
 
 /**
  * A literal string.
@@ -20,13 +24,25 @@ using std::array;
 template <size_t n>
 class string_literal {
  public:
-  constexpr string_literal(const char(&str)[n]) {
+  constexpr string_literal(const char (&str)[n]) {
     std::copy_n(str, n, data_.data());
   }
   constexpr bool operator==(const string_literal& other) const = default;
+  operator string() const {
+    return string{ data_.data(), n };
+  }
+
  private:
   array<char, n> data_;
 };
+
+/* Concept */
+template <typename T>
+struct is_string_literal : std::false_type {};
+template <size_t n>
+struct is_string_literal<string_literal<n>> : std::true_type {};
+template <typename T>
+concept is_string_literal_v = is_string_literal<T>::value;
 
 } // namespace crystal
 
