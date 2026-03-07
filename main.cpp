c049c5acdb6d5cc2ae1615e19a2b6fcc88334723
ray847 <xiachengyi21@163.com>
2026-03-07 22:56:26 +0800

    Remove std::stacktrace

--- main.cpp
+++ main.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
-#include <unordered_set>
-#include <format>
 
-#include <CrystalBase/fixed_string.h>
+#include <CrystalBase/error.h>
 
 int main() {
-	const crystal::fixed_string string{"123"};
-	std::unordered_set<crystal::fixed_string<32>> set;
-	std::cout << string << std::endl;
-	std::cout << std::format("fixed string: {}", string) << std::endl;
-
+  crystal::Error e{"123"};
+  std::cout << e.msg << std::endl;
   return 0;
 }
